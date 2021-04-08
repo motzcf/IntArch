455470fac4135170c4de3c3ea5997f51f377afc3
motzcf <82174417+motzcf@users.noreply.github.com>
2021-04-08 13:41:41 -0400

    Add files via upload

---
+++ Internet_Archaeology/data/WorkflowFeatures.js
@@ -0,0 +1,125 @@
+var json_WorkflowFeatures = {
+"type": "FeatureCollection",
+"features": [
+	{ 
+		"type" : "Feature",
+		"id" : 1,
+		"properties": { 
+			"Stream": "Artifacts",
+			"Box_text": "Temporary container labeled and filled (SU and 'ART')",
+			"PopupText": "<b>Temporary Containers</b> <br> <br> What is a temporary container?"
+		},
+		"geometry": { 
+			"type": "Polygon",
+			"coordinates": [ 
+				[ 
+					[ 7.5, 26 ],
+					[ 7.5, 27 ],
+					[ 8.6, 27 ],
+					[ 8.6, 26 ]
+				]
+			]
+		}
+	},
+	{ 
+		"type": "Feature",
+		"id" : 2,
+		"properties": { 
+			"Stream": "Artifacts",
+			"Box_text": "Temporary container brought from field to lab (except bulk stone)",
+			"PopupText": "<b>HEADER</b><br><br>Text goes here."
+		},
+		"geometry": { 
+			"type": "Polygon",
+			"coordinates": [ 
+				[
+					[ 10, 26 ],
+					[ 10, 27 ],
+					[ 11, 27 ],
+					[ 11, 26 ]
+				]
+			]
+		}
+	},
+	{ 
+		"type": "Feature",
+		"id" : 3,
+		"properties": { 
+			"Stream": "Artifacts",
+			"Box_text": "Contents of temporary containers sorted into categories",
+			"PopupText": "<b>The Artifacts</b><br><br>What are these categories? <i>Pottery, Finds,</i> and <i>Bulk</i>"
+		},
+		"geometry": { 
+			"type": "Polygon",
+			"coordinates": [ 
+				[
+					[ 13, 26 ],
+					[ 13, 27 ],
+					[ 14.1, 27 ],
+					[ 14.1, 26 ]
+				]
+			]
+		}
+	},
+	{ 
+		"type": "Feature",
+		"id" : 4,
+		"properties": { 
+			"Stream": "Artifacts",
+			"Box_text": "Collection Container record created",
+			"PopupText": "<b>HEADER</b><br><br>Text goes here."
+		},
+		"geometry": { 
+			"type": "Polygon",
+			"coordinates": [ 
+				[ 
+					[ 15.5, 29 ],
+					[ 15.5, 30 ],
+					[ 16.5, 30 ],
+					[ 16.5, 29 ]
+				]
+			]
+		}
+	},
+	{ 
+		"type": "Feature",
+		"id" : 5,
+		"properties": { 
+			"Stream": "Artifacts",
+			"Box_text": "Collection Container record created",
+			"PopupText": "<b>HEADER</b><br><br>Text goes here."
+		},
+		"geometry": { 
+			"type": "Polygon",
+			"coordinates": [ 
+				[ 
+					[ 15.5, 26 ],
+					[ 15.5, 27 ],
+					[ 16.5, 27 ],
+					[ 16.5, 26 ]
+				]
+			]
+		}
+	},
+	{ 
+		"type": "Feature",
+		"id" : 6,
+		"properties": { 
+			"Stream": "Artifacts",
+			"Box_text": "Collection Container record created",
+			"PopupText": "<b>HEADER</b><br><br>Text goes here."
+		},
+		"geometry": { 
+			"type": "Polygon",
+			"coordinates": [ 
+				[ 
+					[ 15.5, 22.5 ],
+					[ 15.5, 23.5 ],
+					[ 16.5, 23.5 ],
+					[ 16.5, 22.5 ]
+				]
+			]
+		}
+	}
+]
+}
